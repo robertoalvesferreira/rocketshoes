54e61cd945b26dcdc05acce31c0f9fc9289682a7
robertoalvesferreira <robertoaff06@gmail.com>
2020-03-12 20:36:31 -0300

    Removendo produto

--- src/store/modules/cart/reducer.js
+++ src/store/modules/cart/reducer.js
@@ -12,8 +12,16 @@ export default function cart(state = [], action) {
         } else {
           draft.push({ ...action.product, amount: 1 });
         }
-        // draft.push(action.product);
       });
+    case 'REMOVE_FROM_CART':
+      return produce(state, draft => {
+        const productIndex = draft.findIndex(p => p.id === action.id);
+
+        if (productIndex >= 0) {
+          draft.splice(productIndex, 1);
+        }
+      });
+
     default:
       return state;
   }
